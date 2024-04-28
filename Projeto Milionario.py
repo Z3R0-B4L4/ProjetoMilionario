e640a1fbf107d274189415ccb99ce11e04a6cceb
Z3R0-B4L4 <eduardo9685743210@gmail.com>
2024-04-28 15:37:51 -0300

    Aparentemente o código está funcionando ok, os calculos foram corrigidos, pode conter algun erros ainda que eu não encontrei, caso encontre algum por favor me informe, tentarei corrigi-lo o quanto antes

--- Projeto Milionario.py
+++ Projeto Milionario.py
@@ -1,30 +1,22 @@
-#Esse é um programa que visa lucrar com base em 10% da sua banca, um jeito de lucrar mais lentamente e deixando uma maior chance de voce não acabar o dia estourando sua banca
+#Esse é um programa que visa lucrar com base em 10% da sua banca, um jeito de lucrar mais lentamente e deixando uma maior chance de voce não acabar o dia zerando sua banca
 #caso queira operar ele com algo diferente dos 10% altere na linha 50 o valor "0.1" para quanto você ira operar (cada "0.1" é igual a 10%, ou seja, se você quiser ir pro all-win altere o valor para "1" = 100%  ou se quer ir com metade da banca, para "0.5" = 50%)
-#Esse programa tambem opera com base na tabela de boringer mas não usa todo o lucro como seria normalmente -/- O PROGRAMA PODE PRECISAR DE MELHORIAS NÃO LEVE OS RESULTADOS AO PÉ DA LETRA, OS VALORES REAIS PODEM SER UM POUCO DIFERENTES, UTILIZE-OS APENAS COMO BASE
-
+#Esse programa tambem opera com base na tabela de boringer mas não usa todo o lucro como seria normalmente -/- O PROGRAMA PODE NÃO ESTAR 100% CORRETO
 from datetime import datetime
 from datetime import date
 
-data = datetime.now() # captura a data e horario atual
-df=data.strftime('%d/%m/%Y %H:%M') # formata a data para o padrão brasileiro e mostra o horario junto
-dft=data.strftime('%d/%m/%Y') # formata a data para o padrão brasileiro (data especifica para o total)
+data = datetime.now()
+df=data.strftime('%d/%m/%Y %H:%M') #data com hora para usar nas operações de win e loss
+dtf=data.strftime('%d/%m/%Y') #data sem hora para deixar na linha final com o rendimento e a banca
 
-loop = 0 
-d1 = 0 # "d" = dia
-i=1 # "i" um contador para por limite
-w = 0 # win com a maior % de retorno
-l = 0 # loss dos 50% de cada operação
-rendimento = 0 # rendimento de cada operação
-banca = 0 # banca de cada dia
-x1 = 0 # contador para o break do stop loss
-bd = open('Gerenciamento.txt','a') # bd é tipo um banco de dados, ele utiliza a função open para abrir o aquivo txt com nome Gerenciamento e o parametro "a" que ele recebe após a virgula é para que caso algo seja adicionado nele, que seja adicionado no final do documento
+#DA LINHA 9 ATÉ A LINA 26 VOCÊ PODE APAGAR, ELA É MAIS UM INCENTIVO DO QUE ALGO REALMENTE NECESSÁRIO
 
-# "a" é usado pra saber o total de lucro objetivo naquele dia (1 = segunda, 2 = terça, 3 = quarta, 4 = quinta, 5 = sexta, sabado e domingo não serão dias operaveis, descanse e aproveite a vida)
+# "a" é usado pra saber o total de lucro objetivo naquele dia (sabado e domingo não serão dias operaveis, descanse e aproveite a vida)
 a1=7.59 # necessario de 10x a + pra operar com esse valor ou seja 75.00
 a2=57.64 # necessario de 10x a + pra operar com esse valor ou seja 580.00
 a3=437.70 # necessario de 10x a + pra operar com esse valor ou seja 4380.00
 a4=3323.78 # necessario de 10x a + pra operar com esse valor ou seja 33250.00
 a5=25239.95 # necessario de 10x a + pra operar com esse valor ou seja 252400.00
+
 
 print(f"10% de cada dia caso o lucro seja de 100% nas 10 tentativas\na1 = {a1:.2f}\na2 = {a2:.2f}\na3 = {a3:.2f}\na4 = {a4:.2f}\na5 = {a5:.2f}\n")
 print(f"O lucro total possivel de cada dia é:\nD1 = {a1*10:.2f}\nD2 = {a2*10:.2f}\nD3 = {a3*10:.2f}\nD4 = {a4*10:.2f}\nD5 = {a5*10:.2f}\n")
@@ -34,87 +26,78 @@ print(f"Objetivo é 1 dos a1={a1*10:.2f} a2={a2*10:.2f} a3={a3*10:.2f} a4={a4*10
 print("Não precisa ser hoje nem amanhã, mas um dia o objetivo é alcançado")
 print("AJA COM CALMA E CONFIE NO SEU SONHO QUE VOCÊ IRA ALCANÇAR O SUCESSO 100% DE CERTEZA.\n")
 
-s=10#float(input("Digite sua banca: ")) # <--- BANCA ---><--- BANCA ---><--- BANCA ---><--- BANCA ---><--- BANCA ---><--- BANCA ---><--- BANCA ---><--- BANCA ---><--- BANCA --->
-banca = s
-bd.write(f"{dft} - Banca Inicial: {s}\n")
-while True:
-    loop +=1
-    if loop > 10:
-        break 
-    print(f"{loop}° loop ")
-    if s < 1: # operação minima é 1$
-        print("Pobre, vaza") # Não se ofenda, é só uma brincadeira
+#DA LINHA 9 ATÉ A LINA 26 VOCÊ PODE APAGAR, ELA É MAIS UM INCENTIVO DO QUE ALGO REALMENTE NECESSÁRIO
+
+#definição das variaveis
+loop = 0 # define quantas repetições o programa ira executar antes de se auto encerrar
+entrada = 0 # para a primeira entrada sempre será 10% do valor da banca
+i=1 # contador para interromper caso aja muitas vitorias consecultivas
+win=0 # armazena o valor total da win
+loss=0 # armazena o valor da loss
+rendimento=0 # soma de win e loss
+banca_final = 0 
+stop_loss = 0 # contador de derrotas para que não acabe zerando a banca de uma vez
+
+#abre o arquivo em que ficara salvo as informações
+bd = open('teste.txt','a') 
+
+
+banca_inicial=float(input("Digite o valor da sua banca: R$")) # valor disponivel para seu investimento
+bd.write(f"{dtf} - Banca Inicial: R${banca_inicial}\n")
+#inicio do loop
+while loop < 5:
+    loop+=1
+    #verifica se tem o minimo necessario para operar
+    if banca_inicial < 1:
+        print(f"\nImpossivel operar com esse valor!\n")
         break
-    p = int(input("Digite 1 para prosseguir\n"))
-    if p==1:
-        d1=s*0.1 # 10% da banca
-        banca += -d1
-        print(f"Você deve usar {d1:.2f}")
-        bd.write(f"\n{df} Entrada de: R${d1:.2f}\n")
-        opt = int(input("Você ganhou? 1 para sim 0 para não: "))
-
-        if opt == 0:
-            l += l - d1
-            rendimento += -d1
-            bd.write(f"{df} - loss: R${l:.2f}\n")
-            l = 0
-            x1+=1
-        while opt == 1:
-            d1 = d1*1.5
-            i += 1
-            print(f"Você deve usar {d1:.2f} para a {i}° vez\n")
-            opt = int(input("Você ganhou? 1 para sim 0 para não: "))
-            
-            if opt == 1:
-                w += (d1*1.92)-d1 # Lucro de 92%
-                rendimento += w
-                bd.write(f"{df} - win: R${w:.2f}\n")
-            elif opt == 0:
-                w += (d1*1.92)-d1
-                l += l - d1
-                rendimento += w
-                bd.write(f"{df} - win: R${rendimento:.2f}\n")
-                bd.write(f"{df} - loss: R${l:.2f}\n")
-            w=0
-            l=0
-            if i== 5:
-                i=1
-                print(f"Você ja ganhou o dia!")
-                x1 = 0
-                break
-               
-        if opt == 0:
-            i=1
-            d1= s*0.1
-            print(f"Você deve usar {d1:.2f}\n")
-            bd.write(f"{dft} - Rendimento: {round(rendimento,2)}\n-------------------------------------------------------\n")
-        elif p == 0:
-            w += d1*1.92
-            l += l - d1
-            rendimento += w
-            bd.write(f"{df} - win: R${d1:.2f}\n")
-            bd.write(f"{df} - loss: R${l:.2f}\n")
-            w=0
-            l=0
-
-        if x1 == 3: # Stop loss
-            print("Você ja perdeu muito hoje, va descansar.")
-            break
-    
-    elif p == 0:
-        if loop > 10:
-            break
-        else:
-            print("Indo para o próximo loop\n")
-    
     else:
-        print("opção invalida\n")
-banca += rendimento
-bancaf = s
-rendimento = banca - bancaf
-bd.write(f"{dft} - Rendimento: {rendimento:.2f} / Banca: {(banca):.2f}\n-------------------------------------------------------\n")
-bd.close()
-rendimento = 0
-banca = 0
+        entrada = banca_inicial*0.1
+        print(f"\nVocê deve operar com R${entrada}\n")
+        escolha = int(input("Você ganhou? digite 1 para sim e 0 para não\n"))
+        #escolha de vitoria derrota
+        while escolha == 1: # caso ganhe
+            i+=1
+            win = (entrada*1.92)-entrada # lucro de 92%
+            entrada = entrada*1.5 # entrada equivalente a entrada anterior mutiplicado por 1.5 (50%)
+            rendimento+=win # acrescenta o lucro ao rendimento
+            bd.write(f"Entrada: R${entrada:.2f}\nWin: R${win:.2f}\n") #escreve no arquivo de texto o valor de entrada e o valor do lucro
+            print(f"Você deve usar para próxima operação R${entrada:.2f} na {i}° vez")
+            escolha = int(input("Você ganhou? digite 1 para sim e 0 para não\n"))
+            if escolha == 0 : # caso perca
+                i=1 # reseta o contador de wins consecultivas
+                loss-=entrada # acrescenta a entrada negativa na loss
+                rendimento+=loss # Faz a soma da perca sobre o rendimento
+                #reset de win e loss para que não interfira no próximo loop
+                win = 0 
+                loss = 0
+                stop_loss+=1 #acrescenta a derrota
+                # encerra o loop de escolha
+            if i==5:
+                i=1
+                print(f"Parabens, você ganhou muito!\n")
+                #reset de win e loss para que não interfira no próximo loop
+                win = 0
+                loss = 0
+                #reset do stop loss
+                stop_loss=0
+                break
+        if escolha ==0:
+            loss-=entrada # acrescenta a entrada negativa na loss
+            rendimento+=loss # Faz a soma da perca sobre o rendimento
+            bd.write(f"Entrada: R${entrada:.2f}\nLoss: R${loss:.2f}\n")
+            #reset de win e loss para que não interfira no próximo loop
+            win = 0 
+            loss = 0
+            stop_loss+=1 #acrescenta a derrota
+            # encerra o loop de escolha
+        if stop_loss == 3:
+            print(f"Você ja perdeu o suficiente por hoje, vá descansar.\n")
+            break
 
-# Soma com erro no resultado, não sei como resolver
+
+banca_final +=  rendimento + banca_inicial # faz a soma para mostrar quanto ficou na sua banca após o lucro/Perca
+
+bd.write(f"\n{dtf} - Rendimento: R${rendimento:.2f} / Banca Final: R$ {banca_final:.2f}\n-------------------------------------------------------\n")
+bd.close()
+
